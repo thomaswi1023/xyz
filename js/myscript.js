0828ee0a445beff75b406c277da166e176f5a9b8
thomaswi1023 <thomas.wijaya199@gmail.com>
2016-06-10 09:47:16 +0700

    VERSI1

---
+++ js/myscript.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+$(".button-collapse").sideNav();
+ $('.dropdown-button').dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      constrain_width: false, // Does not change width of dropdown to that of the activator
+      hover: false, // Activate on hover
+      gutter: 0, // Spacing from edge
+      belowOrigin: false, // Displays dropdown below the button
+      alignment: 'left' // Displays dropdown with edge aligned to the left of button
+    }
+  );
+   $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+  });
+  $(document).ready(function() {
+    $('select').material_select();
+  });
+   $('select').material_select('destroy');
+});
